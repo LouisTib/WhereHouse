e8b17d0ab29aaf41e60d96f0b2c6258bc102387c
Louis Tiboldo <louistiboldo@gmail.com>
2026-05-20 13:16:39 -0400

    Updated contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,19 +5,23 @@
 @section('head')
     <link rel="stylesheet" href="{{ asset('css/contact.css') }}?v={{ time() }}">
     <style>
-        body {
+        .contact {
             background-image: url('{{ asset('images/ContactBackground.png') }}');
-            background-size: cover;
         }
     </style>
 @endsection
 
 @section('content')
     <div class="contact">
-        <h1>Contact</h1>
-        <p>Need assistance or have questions? We're here to help! Reach out and our team will
-            respond as quickly as possible to assist you.</p>
-        <a href="mailto:louistiboldo@gmail.com"> Email us</a>
-
+        <div class="contact-card">
+            <div class="contact-content">
+                <h1>Get in Touch</h1>
+                <p>Need assistance or have questions? We're here to help! Reach out to our team and we'll respond as quickly
+                    as possible.</p>
+                <a href="mailto:louistiboldo@gmail.com">
+                    ✉️ Email Us
+                </a>
+            </div>
+        </div>
     </div>
 @endsection
